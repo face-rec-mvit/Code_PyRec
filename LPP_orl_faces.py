1c9dece3bf9d3e24c1c8bbb5da2069cb0db9c900
Kunal Ghosh <kunal.t2@gmail.com>
2011-02-21 22:34:47 -0500

    LPP bootstrap for orl_faces face database

---
+++ LPP_orl_faces.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+from src import lpp
+import numpy as np
+
+res = {0: 's1', 1: 's2', 2: 's3', 3: 's4', 4: 's5', 5: 's6', 6: 's7', 7: 's8', 8: 's9', 9: 's10', 10: 's11', 11: 's12', 12: 's13', 13: 's14', 14: 's15', 15: 's16', 16: 's17', 17: 's18', 18: 's19', 19: 's20', 20: 's21', 21: 's22', 22: 's23', 23: 's24', 24: 's25', 25: 's26', 26: 's27', 27: 's28', 28: 's29', 29: 's30', 30: 's31', 31: 's32', 32: 's33', 33: 's34', 34: 's35', 35: 's36', 36: 's37', 37: 's38', 38: 's39', 39: 's40'}
+
+ImageDir=lpp.Image_Directory("/media/backup/kunal/Face-rec/FACE_DATABASES/orl_faces",res)
