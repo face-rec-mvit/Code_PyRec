f158d470e10f8d7ea62b759b893e7b94500e8d0b
Kunal Ghosh <kunal.t2@gmail.com>
2011-03-03 11:01:13 -0500

    modified get_abs_path_of_all_files_in_directory to add --include and --exclude support

--- src/get_abs_path_of_all_files_in_directory.py
+++ src/get_abs_path_of_all_files_in_directory.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 
 def gen_files(files,top,names):
     for name in names:
@@ -11,11 +12,21 @@ def gen_files(files,top,names):
             files.append(path)
     return files
 
-def get_files(directory,ftype=None):
+def get_files(directory,include=None,exclude=None,ftype=None):
     """
     The function lists all files in the top level
     directory "directory" , by default it returns all the
     file types.
+
+    include takes in a regular expression (as a string)
+    and returns files only matching the include regex
+
+    exclude takes in a regular expression (as a string)
+    and DOES NOT RETURN files that match the exclude regex
+
+    NOTE : Both include and exclude CAN BE applied to gether,
+    in which case INCLUDE is run through FIRST and THEN EXCLUDE
+    AND BOTH USE THE PYTHON REGEX MODULE and ARE CASE SENSITIVE
 
     The can be forced to return only certain file types
     by passing the types as a list , as a second argument.
@@ -31,8 +42,18 @@ def get_files(directory,ftype=None):
         usage:   get_abs_path_of_all_files_in_directory [options] [directory path]
 
         options:
-        comma separated list of file type which must ONLY be returned.
-        example : --types=jpeg,jpg
+        --types :   comma separated list of file type which must ONLY be returned.
+                    example : --types jpeg,jpg
+        --include:  a regular expression of files to include
+                    example : --include "hello(world)?" , matches hello,hello
+                    world
+        --exclude:  a regular expression of files to exclude
+                    example : --exclude "hello(world)?" , exclude hello,hello
+                    world for the list of displayed files
+        NOTE:
+        1. IF include and exclude are both provided then first Include is
+        executed and then Exclude.
+        2. --include "" is treated as IGNORE THE FLAG, --exclude is similar
         """
         return "ERROR : Plese enter a valid directory"
 
@@ -50,7 +71,56 @@ def get_files(directory,ftype=None):
 
     else :
         os.path.walk(directory,gen_files,files)
-        
+    
+    if include != None and len(include) != 0:
+        """
+        KEEP FILES that MATCH the regex 
+        !=0 is to handle the case where the user enters
+        --include "" which is treated as IGNORE THE FLAG
+        """
+        #print 
+        #"""
+        #No error handling is done, Please check re module of
+        #python in case you come across errors.
+        #"""
+
+        regex_include = re.compile(include)#,re.IGNORECASE)#uncomment to make case insensitive
+        included_list=[]
+        for entry in files:
+            result = regex_include.search(str(entry).strip())
+            #the string is stripped() before searching
+            if result :#regex not in filename
+                included_list.append(entry)
+        #print "include is:",include
+        #included_list=[]
+        #for entry in files:
+        #    result = entry.find(include)
+        #    if result >=0 :
+        #        included_list.append(entry)
+        files = included_list
+
+
+    if exclude != None and len(exclude) != 0:
+        """
+        DELETE FILES that MATCH the entry
+        !=0 is to handle the case where the user enters
+        --exclude "" which is treated as IGNORE THE FLAG
+        """
+        #print 
+        #"""
+        #No error handling is done, Please check re module of
+        #python in case you come across errors.
+        #"""
+
+        regex_exclude = re.compile(exclude)#,re.IGNORECASE)#uncomment to make case insensitive
+        excluded_list=[]
+        for entry in files:
+            result = regex_exclude.search(str(entry).strip())
+            #the string is stripped() before searching
+            if not result:
+                excluded_list.append(entry)
+        files = excluded_list
+
     return files
 
 def print_list(l):
@@ -59,24 +129,40 @@ def print_list(l):
 
 if __name__ == '__main__' :
     arg = sys.argv
-
-    if len(arg) == 2:
-        dirname = arg[1]
-        print dirname,2
-        file_list = get_files(dirname,None)
-        print_list(file_list)
-
-    elif len(arg) == 3:
-        dirname = arg[2]
-        if ( arg[1].startswith("--types=" )):
-            arg[1]=arg[1][len("--types="):]
-            ftype=arg[1].split(",")            
-            file_list = get_files(dirname,ftype)
-            print_list(file_list)
-        else:
-            print "Error in --types"#return(1)
-    else:
+    dirname = arg[len(arg)-1]
+    type_list = None
+    includ = None
+    exclud = None 
+    #the last argument is the directory name 
+    
+    if len(arg) == 1:
         print get_files(None)
+    elif len(arg) == 2:
+        file_list = get_files(directory=dirname,ftype=None)
+        print_list(file_list)
+    else:
+        #elif len(arg) == 3:
+        #    dirname = arg[2]
+        #    if ( arg[1].startswith("--types=" )):
+        #        arg[1]=arg[1][len("--types="):]
+        #        typ=arg[1].split(",")            
+        #        file_list = get_files(directory=dirname,ftype=typ)
+        #        print_list(file_list)
+        #    else:
+        #        print "Error in --types"#return(1)
+        if "--types" in arg:
+            type_list = arg[arg.index("--types")+1].split(",")
+            #print type_list
+        if "--include" in arg:
+            includ = str(arg[arg.index("--include")+1])
+            #print type_list,includ,exclud
+        if "--exclude" in arg:
+            exclud = str(arg[arg.index("--exclude")+1])
+            #print type_list,includ,exclud
 
+        print dirname,type_list,includ,exclud
+        file_list = \
+        get_files(directory=dirname,include=includ,exclude=exclud,ftype=type_list)
+        print_list(file_list)
 
 
