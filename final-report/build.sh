f071297784afa24e53092f008e1be33a9ade27ae
Kunal Ghosh <kunal.t2@gmail.com>
2011-05-14 16:25:24 +0530

    consolidating all tex files into one

--- final-report/build.sh
+++ final-report/build.sh
@@ -2,9 +2,9 @@
 rubber --clean -d *.tex
 
 #make the tex files into pdf directly
-rubber -d frontpage.tex
-rubber -d certificate.tex
-rubber -d acknowledgement.tex
+rubber -d main.tex
+#rubber -d certificate.tex
+#rubber -d acknowledgement.tex
 
 #clean the temporary files
-rubber --clean *.tex`
+rubber --clean *.tex
