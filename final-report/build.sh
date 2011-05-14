1da19e22f3576325892d638770d3844747664627
Kunal Ghosh <kunal.t2@gmail.com>
2011-05-14 16:39:58 +0530

    removed auto pdf building

--- final-report/build.sh
+++ final-report/build.sh
@@ -1,10 +1,10 @@
 #clean the build directory
-rubber --clean -d *.tex
+rubber --clean  *.tex
 
 #make the tex files into pdf directly
-rubber -d main.tex
-#rubber -d certificate.tex
-#rubber -d acknowledgement.tex
+rubber  main.tex
+#rubber  certificate.tex
+#rubber  acknowledgement.tex
 
 #clean the temporary files
 rubber --clean *.tex
